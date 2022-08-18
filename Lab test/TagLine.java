061f2723c559d49cf8432fdb73816bcd28120acf
Bhagyashree-5 <111446245+Bhagyashree-5@users.noreply.github.com>
2022-08-18 18:22:43 +0530

    Add files via upload

---
+++ Lab test/TagLine.java
@@ -0,0 +1,28 @@
+class TagLine
+
+//method to print tagline
+static void printTagLine()
+{
+	case "Apple";
+	{
+	System.out.println("Think Different");
+	}
+	case "Adidas";
+	{
+	System.out.println("Adidas is all in");
+	}
+	case "Nike";
+	{
+	System.out.println("Just do it");
+	}
+	case "Calvin Klein";
+	{
+	System.out.println("Deal with it");
+	}
+	case "Cadbury";
+	{
+	System.out.println("Let's do something good");
+	}
+	
+	
+}
